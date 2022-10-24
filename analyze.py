Moved more to preprocessing. Fixes to calculation of team score. Some minor changes.

analyze.py
@@ -49,7 +49,7 @@ class MetagameData:
     Contains data for a provided metagame and performs useful analysis on it.
     """
 
-    def __init__(self, json_file, threat_file):
+    def __init__(self, json_file, threat_file, team_file):
         """Load metagame data from file.
 
         Args:
@@ -68,9 +68,13 @@ class MetagameData:
             self._total_pokes = data["total_pokes"]
             self._pokes_per_team = data["pokes_per_team"]
             self._num_teams = data["num_teams"]
+            self._total_pairs = data["total_pairs"]
 
         with open(threat_file, "rb") as file:
             self._threat_matrix = np.load(file)
+
+        with open(team_file, "rb") as file:
+            self._team_matrix = np.load(file)
 
     def _find_threats(self, team):
         """Generate threat ratings for threats for a provided team.
@@ -99,44 +103,8 @@ class MetagameData:
             Note that it can be a non-integer as Smogon weights statistics by
             player rating.
         """
+        # This should probably be precalculated.
         return sum(self.pokemon[poke]["Abilities"].values())
-
-    def _p_x_given_y(self, x, y):
-        """Calculate probability that x appears on a team if y does.
-
-        Args:
-            x (str): Name of a Pokemon.
-
-            y (str): Name of a Pokemon.
-
-        Returns:
-            float [0, 1]: Probability(x is on a team|y is on that team)
-        """
-        if y not in self.pokemon[x]["Teammates"]:
-            return 0  # Never appear together
-
-        return self.pokemon[x]["Teammates"][y] / self.count_pokemon(y)
-
-    def _p_x_given_not_y(self, x, y):
-        """Calculate probability that x appears on a team if y does not.
-
-        Args:
-            x (str): Name of a Pokemon.
-
-            y (str): Name of a Pokemon.
-
-        Returns:
-            float [0, 1]: Probability(x is on a team|y is not on that team)
-        """
-        if y not in self.pokemon[x]["Teammates"]:
-            # Never appear together, so P(x|not y) = p(x)
-            return self.count_pokemon(x) / self._num_teams
-
-        # P(x|not y) = P(x and not y) / P(not y)
-        # (x - (x U y)) is count of times x appeared without y
-        count_x_and_not_y = self.count_pokemon(x) - self.pokemon[x]["Teammates"][y]
-        pokes_except_y = self._num_teams - self.count_pokemon(y)
-        return count_x_and_not_y / pokes_except_y
 
     def _get_counter_score(self, threats, team_size, poke):
         """Generate a score based on how good a Pokemon \
@@ -180,16 +148,10 @@ class MetagameData:
             sum_usage_factor += usage_factor
             # P(X|not Y) is 0 if X doesn't occur without Y.
             # So Y needs to be on team.
-            given_not = self._p_x_given_not_y(poke, mon)
-            assert(0 <= given_not <= 1)
-            if given_not == 0:
-                ratio = math.inf
-            else:
-                given = self._p_x_given_y(poke, mon)
-                assert(0 <= given <= 1)
-                ratio = given / given_not
-            # On the flipside, if P(X|Y) is 0, we need to not put Y on the team.
+            # On the flip side, if P(X|Y) is 0, we need to not put Y on the team.
             # This is common where X and Y are the same mon.
+
+            ratio = self._team_matrix[self._indices[mon], self._indices[poke]]
             if ratio == 0:
                 # This behaves the same way multiplying by 0 would in the non-log form.
                 log_sum = -math.inf
@@ -328,7 +290,7 @@ class MetagameData:
                 c_scores[index], t_scores[index], u_scores[index], weights)
 
             scores.append((poke, combined, c_scores[index],
-                            t_scores[index], u_scores[index]))
+                t_scores[index], u_scores[index]))
 
         # TODO consider refactoring to return a dictionary.
         # Maybe dict (name -> a scores object)
@@ -353,7 +315,7 @@ class MetagameData:
         return sorted(scores, key=lambda kv: -kv[1])[0][0]
 
     def _get_best_with_improvement(self, team, current, weights):
-        """Find the best swap for a Pokemon on a team
+        """Find the best swap for a Pokemon on a team.
 
         Args:
             team (list of str): Names of Pokemon already on the team, except the one to swap.
@@ -403,7 +365,7 @@ class MetagameData:
         teams = set(tuple(sorted(my_team)))
         while change_made:
             change_made = False
-            most_improvement = (None, 0, -1) # Mon to use, score, index of swap.
+            most_improvement = (None, 0, -1)  # Mon to use, score, index of swap.
             # Start at len(team) to leave Pokemon the user specified in.
             for x in range(len(team), 6):
                 old_member = my_team[x]

preprocess.py
@@ -2,18 +2,20 @@
 
 import ujson as json
 import numpy as np
+import math
 
 COUNTER_COVERED_FACTOR = 1
 MIN_USAGE = 0.0015
 
-def prepare_files(json_file, threat_file):
+
+def prepare_files(json_file, threat_file, teammate_file):
     """Validate and pre-process files directly from Smogon.
 
     Removes Pokemon with extremely low usage.
     Removes Pokemon without counters data.
     Trims out counters data for Pokemon that are no longer included.
     Verifies that there is a reasonable number of Pokemon remaining.
-    Builds a threat matrix.
+    Builds a threat matrix and a teammate matrix.
 
     Args:
         json_file (str): Path to file containing raw version of
@@ -22,6 +24,11 @@ def prepare_files(json_file, threat_file):
         threat_file (str): Path to save the numpy threat matrix to.
         File will be overwritten.
         threat_matrix[x, y] is how threatening the Pokemon with index
+        y is to the Pokemon with index x.
+        
+        teammate_file (str): Path to save the numpy teammate matrix to.
+        File will be overwritten.
+        team_matrix[x, y] is how good a teammate the Pokemon with index
         y is to the Pokemon with index x.
 
     Returns:
@@ -69,18 +76,41 @@ def prepare_files(json_file, threat_file):
         data["pokes_per_team"] = total_team_members / total_pokes + 1
         data["num_teams"] = total_pokes / data["pokes_per_team"]
 
-    with open(threat_file, "wb") as file:
-        threat_matrix = np.empty((len(pokemon), len(pokemon)))
-        for index, mon in enumerate(pokemon):
-            for column, c_mon in enumerate(pokemon):
-                threat_matrix[index, column] = \
-                    _threat_for_poke(pokemon, c_mon, mon)
+        total_pairs = 0
+        for poke in pokemon:
+            total_pairs += sum(pokemon[poke]["Teammates"].values())
 
+        data["total_pairs"] = total_pairs  # TODO maybe div by 2? Currently counts both ways.
+
+    threat_matrix = np.empty((len(pokemon), len(pokemon)))
+    for index, mon in enumerate(pokemon):
+        for column, c_mon in enumerate(pokemon):
+            threat_matrix[index, column] = \
+                _threat_for_poke(pokemon, c_mon, mon)
+
+    with open(threat_file, "wb") as file:
         np.save(file, threat_matrix)
 
-    # Clear the raw checks/counters data since we only need the matrix.
+    total_pairs = 0
+    for mon in pokemon:
+        total_pairs += sum(pokemon[mon]["Teammates"].values())  # TODO if we save this sum we can speed up some other operations
+
+    team_matrix = np.empty((len(pokemon), len(pokemon)))
+    for index, mon in enumerate(pokemon):
+        for column, c_mon in enumerate(pokemon):
+            denom = _p_x_given_not_y(pokemon, c_mon, mon, total_pairs)
+            if denom == 0:
+                team_matrix[index, column] = math.inf
+            else:
+                team_matrix[index, column] = _p_x_given_y(pokemon, c_mon, mon) / denom
+
+    with open(teammate_file, "wb") as file:
+        np.save(file, team_matrix)
+
+    # Clear the raw data since we only need the matrices.
     for poke in pokemon:
         del pokemon[poke]["Checks and Counters"]
+        del pokemon[poke]["Teammates"]
 
     with open(json_file, "w", encoding="utf-8") as file:
         json.dump(data, file)
@@ -117,3 +147,43 @@ def _threat_for_poke(pokemon, threat, poke):
         # Don't need one counter for every mon that's weak to something.
         strength *= COUNTER_COVERED_FACTOR
     return strength * weight
+
+
+def _p_x_given_y(pokemon, x, y):
+    """Calculate probability that x appears in a slot if y is on team.
+
+    Args:
+        x (str): Name of a Pokemon.
+
+        y (str): Name of a Pokemon.
+
+    Returns:
+        float [0, 1]: Probability(x is in a slot|y is on that team)
+    """
+    if y not in pokemon[x]["Teammates"]:
+        return 0  # Never appear together.
+
+    return pokemon[x]["Teammates"][y] / sum(pokemon[y]["Teammates"].values())
+
+
+def _p_x_given_not_y(pokemon, x, y, total_pairs):
+    """Calculate probability that x appears on a slot if y does not.
+
+    Args:
+        x (str): Name of a Pokemon.
+
+        y (str): Name of a Pokemon.
+
+    Returns:
+        float [0, 1]: Probability(x is on a team|y is not on that team)
+    """
+
+    if y not in pokemon[x]["Teammates"]:
+        # Never appear together, so P(x|not y) = p(x)
+        return sum(pokemon[x]["Teammates"].values()) / total_pairs
+
+    # P(x|not y) = P(x and not y) / P(not y)
+    # (x - (x U y)) is count of times x appeared without y
+    count_x_and_not_y = sum(pokemon[x]["Teammates"].values()) - pokemon[x]["Teammates"][y]
+    count_not_y = total_pairs - sum(pokemon[y]["Teammates"].values())
+    return count_x_and_not_y / count_not_y