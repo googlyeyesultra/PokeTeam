a7ef036eebcb622adf6d40fa2a792f0e113abcb2
googlyeyesultra <googlyeyesultra@gmail.com>
2023-06-21 19:34:18 -0500

    Additional tests for Weights and updated test data files.

---
+++ tests/test_weights.py
@@ -0,0 +1,18 @@
+import unittest
+from analyze import Weights
+
+
+class WeightsTestCase(unittest.TestCase):
+    def test_weights(self):
+        weights = Weights(1, 2, 3)
+        self.assertEqual(weights.counter, 1)
+        self.assertEqual(weights.team, 2)
+        self.assertEqual(weights.usage, 3)
+        self.assertEqual(weights.total, 6)
+
+    def test_all_zero(self):
+        weights = Weights(0, 0, 0)
+        self.assertEqual(weights.counter, weights.team)
+        self.assertEqual(weights.team, weights.usage)
+        self.assertNotEqual(weights.counter, 0)
+        self.assertEqual(weights.counter + weights.team + weights.usage, weights.total)
